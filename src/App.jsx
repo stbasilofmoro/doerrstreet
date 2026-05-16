ca1698b55d75719d3cd8b1e168d5eca0b618efd4
Basil Polivka <basil@tiedisposal.com>
2026-05-15 21:17:26 -0700

    Update assets and homepage styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Navbar }       from './components/Navbar'
 import { HomePage }     from './pages/HomePage'
 import { AboutPage }    from './pages/AboutPage'
@@ -8,21 +8,55 @@ import { ContactPage }  from './pages/ContactPage'
 import { LoginPage }    from './pages/LoginPage'
 import './App.css'
 
+const PATH_TO_PAGE = {
+  '/':         'home',
+  '/about':    'about',
+  '/services': 'services',
+  '/team':     'team',
+  '/contact':  'contact',
+  '/login':    'login',
+}
+
+const PAGE_TO_PATH = {
+  home:     '/',
+  about:    '/about',
+  services: '/services',
+  team:     '/team',
+  contact:  '/contact',
+  login:    '/login',
+}
+
+function getPageFromPath() {
+  return PATH_TO_PAGE[window.location.pathname] ?? 'home'
+}
+
 export default function App() {
-  const [page, setPage] = useState('home')
+  const [page, setPage] = useState(getPageFromPath)
+
+  const navigate = (newPage) => {
+    const path = PAGE_TO_PATH[newPage] ?? '/'
+    window.history.pushState({}, '', path)
+    setPage(newPage)
+  }
+
+  useEffect(() => {
+    const onPop = () => setPage(getPageFromPath())
+    window.addEventListener('popstate', onPop)
+    return () => window.removeEventListener('popstate', onPop)
+  }, [])
 
   const pages = {
-    home:     <HomePage     onNavigate={setPage} />,
-    about:    <AboutPage    onNavigate={setPage} />,
-    services: <ServicesPage onNavigate={setPage} />,
-    team:     <TeamPage     onNavigate={setPage} />,
-    contact:  <ContactPage  onNavigate={setPage} />,
-    login:    <LoginPage    onNavigate={setPage} />,
+    home:     <HomePage     onNavigate={navigate} />,
+    about:    <AboutPage    onNavigate={navigate} />,
+    services: <ServicesPage onNavigate={navigate} />,
+    team:     <TeamPage     onNavigate={navigate} />,
+    contact:  <ContactPage  onNavigate={navigate} />,
+    login:    <LoginPage    onNavigate={navigate} />,
   }
 
   return (
     <div className="site-root">
-      <Navbar onNavigate={setPage} currentPage={page} />
+      <Navbar onNavigate={navigate} currentPage={page} />
       {pages[page] ?? pages.home}
     </div>
   )
